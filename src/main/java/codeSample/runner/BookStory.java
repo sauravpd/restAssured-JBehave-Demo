ec725f1cbe5f2df2ecc81b174f830e43683aa974
saurav <sauravpd58@gmail.com>
2018-08-08 01:03:03 +0530

    commit changes in BookStory

--- src/main/java/codeSample/runner/BookStory.java
+++ src/main/java/codeSample/runner/BookStory.java
@@ -3,7 +3,13 @@ package codeSample.runner;
 import java.util.Arrays;
 import java.util.List;
 
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.junit.JUnitStory;
+import org.jbehave.core.reporters.Format;
+import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.CandidateSteps;
 import org.jbehave.core.steps.InstanceStepsFactory;
 
@@ -11,22 +17,22 @@ import codeSample.steps.BookApiSteps;
 
 public class BookStory extends JUnitStories
 {
-	public BookStory() 
-	{
-		super();
-		this.configuredEmbedder().candidateSteps().add(new BookApiSteps());
-	}
-	
 	@Override
-	public List<CandidateSteps> candidateSteps()
-	{
-		return new InstanceStepsFactory(configuration(),this).createCandidateSteps();
-	}
-	
-	@Override
-	protected List<String> storyPaths() {
-	
-		return Arrays.asList("codeSample/story/getBookDetails.story");
+	public Configuration configuration() {
+		return new MostUsefulConfiguration()
+				.useStoryReporterBuilder(
+						new StoryReporterBuilder().withDefaultFormats()
+								.withFormats(Format.HTML, Format.CONSOLE));
 	}
 
+	@Override
+	public List<CandidateSteps> candidateSteps() {
+		return new InstanceStepsFactory(configuration(), new BookApiSteps())
+				.createCandidateSteps();
+	}
+
+	@Override
+	protected List<String> storyPaths() {
+		return Arrays.asList("codeSample/story/getBookDetails.story");
+	}
 }
